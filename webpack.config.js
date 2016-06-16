416a73ecedd20445aee4f44a73a98895cd99f134
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-06-16 19:37:29 -0400

    Add webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* jshint browserify: true */
+
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    Bakery: './static/js/components/Bakery.jsx'
+  },
+  output: {
+    path: __dirname + '/static/build',
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel'
+    },{
+      test: /\.scss$/,
+      loader: 'style!css!autoprefixer!sass?sourceMap'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  externals: {
+    react: 'React'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+  ]
+};
