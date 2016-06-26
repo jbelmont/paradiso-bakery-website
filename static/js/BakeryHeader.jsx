d0f7f98c4974b5602704da24764e71b8e5adb082
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-06-25 20:41:42 -0400

    Add correct filename for sass partial.

--- static/js/BakeryHeader.jsx
+++ static/js/BakeryHeader.jsx
@@ -6,9 +6,9 @@ export default class BakeryHeader extends Component {
     return (
       <header className="bakery__header-container">
         <ul className="nav nav-tabs">
-          <li role="presentation" className="active"><a href="#">Home</a></li>
-          <li role="presentation"><a href="#">Breads & Pastries</a></li>
-          <li role="presentation"><a href="#">Contact</a></li>
+          <li role="presentation" className="active bakery__tabs"><a href="#">Home</a></li>
+          <li role="presentation" className="bakery__tabs"><a href="#">Breads & Pastries</a></li>
+          <li role="presentation" className="bakery__tabs"><a href="#">Contact</a></li>
         </ul>
       </header>
     );
