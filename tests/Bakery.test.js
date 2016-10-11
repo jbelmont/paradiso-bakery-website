8500b78d675b636c4ae2490b5abe48e2da3bdfa9
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-10-11 18:15:29 -0400

    Add react preset to babel

--- tests/Bakery.test.js
+++ tests/Bakery.test.js
@@ -1,14 +1,12 @@
-jest.unmock('../BasicPage');
-
-import TestUtils from 'react-addons-test-utils';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import React from 'react';
 
 import MenuSideBar from '../static/js/components/MenuSideBar.jsx';
 
 test('Show Links in Main bakery site', () => {
   const menuitems = ['Breakfast', 'Breakfast Ala Carte','Boxed Lunches', 'Entree Salads', 'Gourmet Trays and Appetizers', 'Main Selections', 'Pizzas'];
-  const page = TestUtils.renderIntoDocument(<MenuSideBar menuitems={menuitems} />);
-  const button = TestUtils.findRenderedDOMComponentWithTag(page, 'button');
-  expect(ReactDOM.findDOMNode(button).textContent).toBe('Login');
+  const tree = renderer.create(
+    <MenuSideBar menuitems={menuitems} />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
