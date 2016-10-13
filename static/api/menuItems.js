df5772f53700326b5937a482c7814106c148e0d9
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-10-12 20:51:03 -0400

    Add comment to menuItems.js

--- static/api/menuItems.js
+++ static/api/menuItems.js
@@ -1,4 +1,4 @@
-const _recipes = JSON.parse(document.getElementById('recipeData').value);
+const _recipes = JSON.parse(document.getElementById('recipeData').value); // get recipes in serialized form
 
 export default {
   recipes: _recipes
