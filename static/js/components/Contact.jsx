84070dd269b79a421ea2a2c6b25edd0af7bb9074
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-10-22 10:29:10 -0400

    Added sass files to repo.

--- static/js/components/Contact.jsx
+++ static/js/components/Contact.jsx
@@ -4,11 +4,20 @@ class Contact extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      addressInfo: [
+        {addr: '513.231.2780', class: 'contact__container-telephone'}, 
+        {addr: '6732 Clough Pike Cincinnati, OH 45244', class: 'contact__container-address'}, 
+        {addr: 'info@jeanpaulsparadisocafe.com', class: 'contact__container-email'}
+      ]
+    }
   }
   render() {
+    const {addressInfo} = this.state;
+    const info = (addressInfo.map(info => <div className={info["class"]}>{info["addr"]}</div>))
     return (
       <div className="contact__container">
-       Contacts listed here.
+        {info}
       </div>
     );
   }
